b339499f68dd84923bda42b390ff646a439f6b8d
Mohammed Alzakariya <lanhikarixx@gmail.com>
2025-11-05 08:16:35 +0300

    update repro004 and remove unnecessary dependency to lan_rs_common

--- src/repro_tracked/repro004_regex_tester.rs
+++ src/repro_tracked/repro004_regex_tester.rs
@@ -1,6 +1,43 @@
 use clap::{Arg, ArgAction, ArgMatches, Command, command};
-use lan_rs_common::util::regex as comm_regex;
 use regex::Regex;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum RegexCaptureOnceError {
+    #[error("No match for regex {1:?} and string {0:?}")]
+    NoMatch(String, String),
+
+    #[error("Failed to capture for regex {1:?} and string {0:?}")]
+    CaptureError(String, String),
+
+    #[error("Failed to collect capture groups for regex {1:?} and string {0:?}")]
+    CaptureCollectError(String, String),
+}
+
+pub fn regex_capture_once(s: &str, re: &Regex) -> Result<Vec<String>, RegexCaptureOnceError> {
+    type FnErr = RegexCaptureOnceError;
+
+    if !re.is_match(s) {
+        return Err(FnErr::NoMatch(s.to_owned(), re.as_str().to_owned()));
+    }
+
+    let matches = re
+        .captures(&s)
+        .ok_or(FnErr::CaptureError(
+            s.to_owned(),
+            re.as_str().to_owned(),
+        ))?
+        .iter().collect::<Option<Vec<_>>>()
+        .ok_or(FnErr::CaptureCollectError(
+            s.to_owned(),
+            re.as_str().to_owned(),
+        ))?;
+
+    Ok(matches
+        .into_iter()
+        .map(|m| m.as_str().to_owned())
+        .collect::<Vec<_>>())
+}
 
 fn parse_args() -> ArgMatches {
     command!()
@@ -18,28 +55,13 @@ fn parse_args() -> ArgMatches {
 pub fn main() {
     let matches = parse_args();
 
-    let regex = {
-        matches //_
-            .get_one::<String>("regex")
-            .unwrap()
-            .to_string()
-    };
+    let regex = matches.get_one::<String>("regex").unwrap().to_string();
 
-    let str = {
-        matches //_
-            .get_one::<String>("str")
-            .unwrap()
-            .to_string()
-    };
+    let str = matches.get_one::<String>("str").unwrap().to_string();
 
     let re = Regex::new(&regex).expect("Invalid regex");
 
-    println!("regex: {regex}");
-    println!("str: {str}");
-
-    let matches = //_
-        comm_regex::regex_capture_once(&str, &re)
-            .expect("Could not parse regex");
+    let matches = regex_capture_once(&str, &re).expect("Could not parse regex");
 
     println!("matches: {matches:?}");
 }
